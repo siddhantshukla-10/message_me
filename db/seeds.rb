5305d4628f8956eb891df3fa385a68d914f2f284
siddhant <siddhant10shukla@gmail.com>
2019-08-10 01:46:48 +0530

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "siddhant", password: "password")
+User.create(username: "shreya", password: "password")
+User.create(username: "atul", password: "password")
+User.create(username: "shobha", password: "password")
+User.create(username: "vinayak", password: "password")
+
+
+Message.create(body: "Wassup people?", user_id: 2)
+Message.create(body: "Nothing much, you tell?", user_id: 3)
+Message.create(body: "Boref af", user_id: 4)
+Message.create(body: "Holidays are boring...", user_id: 5)
+
+
