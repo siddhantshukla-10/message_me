81fe895819088a3291f46e5f91b6c0e182690598
siddhant <siddhant10shukla@gmail.com>
2019-08-10 06:08:38 +0530

    Add method to broadcast messages and broadcast messages realtime

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,20 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function(){
+  if ($('#messages').length > 0){
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+// submit_message = function(){
+//   $('#message_body').on('keydown',function(e){
+//     if(e.keyCode == 13){
+//       $('button').click();
+//     }
+//   });
+// };
+
 $(document).on('turbolinks:load',function(){
 	$('.ui.dropdown')
 	  .dropdown()
@@ -30,6 +44,8 @@ $(document).on('turbolinks:load',function(){
     ;
   })
 	;
+  // submit_message();
+  scroll_bottom();
 
 });
 
